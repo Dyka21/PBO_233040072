b5e473f737e679442e56d4aaee0dc6028977f4c7
Dyka21 <andikakhaerulana21@gmail.com>
2025-06-10 13:17:48 +0700

    iyh

---
+++ pertemuan15/latihan1_2/HelloWorldForm2.java
@@ -0,0 +1,13 @@
+package pertemuan15.latihan1_2;
+
+import javax.swing.*;
+
+public class HelloWorldForm2 extends JFrame {
+    public HelloWorldForm2() {
+        JLabel label = new JLabel("Hello World!");
+        add(label);
+        setTitle("Latihan 1-2");
+        setSize(200, 100);
+    }
+}
+
---
+++ pertemuan15/latihan1_2/ProgramUtama.java
@@ -0,0 +1,8 @@
+package pertemuan15.latihan1_2;
+
+public class ProgramUtama {
+    public static void main(String[] args) {
+        HelloWorldForm2 form = new HelloWorldForm2();
+        form.setVisible(true);
+    }
+}
